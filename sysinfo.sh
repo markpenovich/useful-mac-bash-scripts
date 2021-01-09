21238d6c2d7421b3b1c2e7702740457b7c986f38
Mark Penovich <Mark@Marks-MacBook-Pro.local>
2021-01-08 19:33:09 -0600

    add more information sections

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+echo -e "\033[31;43m***** HOSTNAME INFORMATION *****\033[0m"
+system_profiler SPSoftwareDataType
+
 echo -e "\033[31;43m***** CPU INFORMATION *****\033[0m"
 sysctl -n machdep.cpu.brand_string
+
+echo -e "\033[31;43m***** FILE SYSTEM DISKSPACE USE *****\033[0m"
+df -h
+
+echo -e "\033[31;43m***** MEMORY INFORMATION *****\033[0m"
+system_profiler SPHardwareDataType | grep "  Memory:"
+system_profiler SPHardwareDataType | grep Cores:
+system_profiler SPHardwareDataType | grep Processors:
+
+echo -e "\033[31;43m***** SYSTEM UPTIME AND LOAD *****\033[0m"
+uptime
+
